4a4023116cf5b1c3a4b3f259b16262c87f976f9e
giveMepower94 <Kul.vova2009@yandex.ru>
2024-05-18 07:21:22 +0300

    Регистрация модели в админке, кастомизация админ панели

--- my_career/portfolio/admin.py
+++ my_career/portfolio/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Client
 
-# Register your models here.
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'phone', 'status', 'created_at')
+    search_fields = ('first_name', 'last_name', 'email', 'phone')
+    list_filter = ('status', 'created_at')
+    fields = ('first_name', 'last_name', 'email', 'phone', 'resume', 'status', 'created_at')
+    readonly_fields = ('created_at',)
+
