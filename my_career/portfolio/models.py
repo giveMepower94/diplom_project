16c3dc78918a4205531e566a856b3ef9ccfbcd1c
giveMepower94 <Kul.vova2009@yandex.ru>
2024-05-18 06:58:21 +0300

    Создание модели Client

--- my_career/portfolio/models.py
+++ my_career/portfolio/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    class Status(models.IntegerChoices):
+        UNCHECKED = 0, 'Не просмотрено'
+        CHECKED = 1, 'Просмотрено'
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, blank=True)
+    resume = models.TextField(blank=True)
+    status = models.IntegerField(choices=Status.choices, default=Status.UNCHECKED)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'client'
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
